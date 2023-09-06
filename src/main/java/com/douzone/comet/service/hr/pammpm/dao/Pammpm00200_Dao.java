8f495bddb5d4ba1511e618d3f3f9d8c96359b1cd
minaci <minaci_@naver.com>
2023-09-06 16:41:39 +0900

    최종 dao

--- src/main/java/com/douzone/comet/service/hr/pammpm/dao/Pammpm00200_Dao.java
+++ src/main/java/com/douzone/comet/service/hr/pammpm/dao/Pammpm00200_Dao.java
@@ -8,14 +8,15 @@ import org.springframework.stereotype.Repository;
 
 import com.douzone.comet.jdbc.mybatis.DzMybatisSupport;
 import com.douzone.comet.service.hr.pammpm.models.Pammpm00200_Model;
-/** 
-  * @description :
-  * @Since   : 
-  * @Author  :
-  * @History :
-  */
+
+/**
+ * @description :
+ * @Since :
+ * @Author :
+ * @History :
+ */
 @Repository
-public class Pammpm00200_Dao  {
+public class Pammpm00200_Dao {
 
 	private DzMybatisSupport mybatisSupport;
 
@@ -24,57 +25,61 @@ public class Pammpm00200_Dao  {
 		this.mybatisSupport = mybatisSupport;
 	}
 
-	//마스터복수 데이터 목록 조회
+	// 마스터복수 데이터 목록 조회
 	public List<Pammpm00200_Model> Pammpm00200_selectAllData(HashMap<String, Object> parameter) throws Exception {
 		return this.mybatisSupport.selectList(this.getClass().getName() + ".Pammpm00200_selectAllData", parameter);
 	}
-	
-	//서브복수 데이터 목록 조회
-	public List<HashMap<String,Object>> Pammpm00200_selectHrMtnityidnfDtl(HashMap<String, Object> parameter) throws Exception {
-		return this.mybatisSupport.selectList(this.getClass().getName() + ".Pammpm00200_selectHrMtnityidnfDtl", parameter);
+
+	// 서브복수 데이터 목록 조회
+	public List<HashMap<String, Object>> Pammpm00200_selectHrMtnityidnfDtl(HashMap<String, Object> parameter)
+			throws Exception {
+		return this.mybatisSupport.selectList(this.getClass().getName() + ".Pammpm00200_selectHrMtnityidnfDtl",
+				parameter);
 	}
 
-	/**
-	 * 단일 데이터 조회
-	 * @param pammpm00200_Model
-	 * @return List<Pammpm00200_Model>
-	 */
-	public Pammpm00200_Model selectPammpm00200_Model(Pammpm00200_Model pammpm00200_Model) throws Exception {
-		return this.mybatisSupport.selectOne(this.getClass().getName() + ".selectPammpm00200_Model", pammpm00200_Model);
+	// 중복 마스터그리드 데이터 조회
+	public Integer selectMstPK(Pammpm00200_Model parameters) throws Exception {
+		return this.mybatisSupport.selectOne(this.getClass().getName() + ".selectMstPK", parameters);
 	}
 
-	/**
-	 * 단일 데이터 입력 처리
-	 * @param pammpm00200_Model
-	 */
-	public void insertPammpm00200_Model(Pammpm00200_Model pammpm00200_Model) throws Exception {
-		this.mybatisSupport.insert(this.getClass().getName() + ".insertPammpm00200_Model", pammpm00200_Model);
+	// 중복 서브그리드 데이터 조회
+	public Integer selectDtlPK(Pammpm00200_Model parameters) throws Exception {
+		return this.mybatisSupport.selectOne(this.getClass().getName() + ".selectDtlPK", parameters);
 	}
 
-	/**
-	 * 단일 데이터 수정 처리
-	 * @param pammpm00200_Model
-	 */
-	public void updatePammpm00200_Model(Pammpm00200_Model pammpm00200_Model) throws Exception {
-		this.mybatisSupport.update(this.getClass().getName() + ".updatePammpm00200_Model", pammpm00200_Model);
+	// 마스터그리드 입력 처리
+	public void insertMstBatch(List<Pammpm00200_Model> parameters) throws Exception {
+		this.mybatisSupport.insertBatch(this.getClass().getName() + ".insertMstBatch", parameters);
 	}
 
-	/**
-	 * 단일 데이터 삭제 처리
-	 * @param pammpm00200_Model
-	 */
-	public void deletePammpm00200_Model(Pammpm00200_Model pammpm00200_Model) throws Exception {
-		this.mybatisSupport.delete(this.getClass().getName() + ".deletePammpm00200_Model", pammpm00200_Model);
+	// 사업장 데이터 입력처리
+	public void insertBizreaInfo() throws Exception {
+		this.mybatisSupport.insert(this.getClass().getName() + ".insertBizreaInfo");
 	}
 
-	public List<HashMap<String, Object>> selectPammpm00200_ModelList(HashMap<String, Object> parameter) {
-		// TODO Auto-generated method stub
-		return null;
+	// 서브그리드 입력 처리
+	public void insertDtl(Pammpm00200_Model parameters) throws Exception {
+		this.mybatisSupport.insert(this.getClass().getName() + ".insertDtl", parameters);
 	}
 
-	public List<Pammpm00200_Model> pammpm00200_service_list(HashMap<String, Object> parameter) {
-		// TODO Auto-generated method stub
-		return null;
+	// 마스터 업데이트 처리
+	public void updateMstBatch(List<Pammpm00200_Model> parameters) throws Exception {
+		this.mybatisSupport.updateBatch(this.getClass().getName() + ".updateMstBatch", parameters);
+	}
+
+	// 서브 업데이트 처리
+	public void updateDtlBatch(List<Pammpm00200_Model> parameters) throws Exception {
+		this.mybatisSupport.updateBatch(this.getClass().getName() + ".updateDtlBatch", parameters);
+	}
+
+	// 마스터 삭제 처리
+	public void deleteMstBatch(List<Pammpm00200_Model> parameters) throws Exception {
+		this.mybatisSupport.deleteBatch(this.getClass().getName() + ".deleteMstBatch", parameters);
+	}
+
+	// 서브그리드 삭제 처리
+	public void deleteDtl(Pammpm00200_Model parameters) throws Exception {
+		this.mybatisSupport.delete(this.getClass().getName() + ".deleteDtl", parameters);
 	}
 
 }
